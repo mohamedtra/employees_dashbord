e452a3bbdbed1ae4874fffc5955aab5c6f90d736
mohamedtra <36902772+mohamedtra@users.noreply.github.com>
2021-03-25 18:19:32 +0100

    change db url

--- NodeJS/db.js
+++ NodeJS/db.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/CrudDB', (err) => {
+mongoose.connect('mongodb://database:27017/CrudDB', (err) => {
 
     if (!err)
         console.log('MongoDB connection succeeded.');
